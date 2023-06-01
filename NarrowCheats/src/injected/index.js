30a6158d752710decd78df3b4dcb483331c2e58d
yeemi routh <Shade4Keal@gmail.com>
2023-06-01 12:16:06 +1000

    module utils

--- NarrowCheats/src/injected/index.js
+++ NarrowCheats/src/injected/index.js
@@ -3,7 +3,25 @@ window.CheatSDK.GameRenderer = window.GameRenderer;
 window.CheatSDK.Settings = {
     Chams: {
         enabled: true,
-        keybind: "z"
+        keybind: "z",
+
+        onFrame: function (a1, a2) {
+            Game.ForeachPlayer(function (plr) {
+                if (plr.renderOrder == 0) {
+                    plr.renderOrder = Infinity;
+                    plr.material[0].depthTest = false;
+                }
+            })
+        },
+
+        onDisabled: function () {
+            Game.ForeachPlayer(function (plr) {
+                if (plr.renderOrder == Infinity) {
+                    plr.renderOrder = 0;
+                    plr.material[0].depthTest = true;
+                }
+            })
+        }
     },
     ArrayList: {
         enabled: true,
@@ -11,46 +29,34 @@ window.CheatSDK.Settings = {
 
         onFrame: function (width, height) {
             let curY = 170;
-            for (var table in window.CheatSDK.Settings) {
-                if (window.CheatSDK.Settings.hasOwnProperty(table)) {
-                    var current = window.CheatSDK.Settings[table];
-
-                    if (current.keybind === undefined) {
-                        window.CheatSDK.GameRenderer.drawText(`${table}: ${current.enabled}`, width - 200, curY, 16, "blue");
-                    }
-                    else {
-                        window.CheatSDK.GameRenderer.drawText(`${table}[${current.keybind}]: ${current.enabled}`, width - 200, curY, 16, "blue");
-                    }
-                    curY += 24;
+            Game.ForeachSetting(function (current, table) {
+                if (current.keybind === undefined) {
+                    window.CheatSDK.GameRenderer.drawText(`${table}: ${current.enabled}`, width - 200, curY, 16, "blue");
                 }
-            }
+                else {
+                    window.CheatSDK.GameRenderer.drawText(`${table}[${current.keybind}]: ${current.enabled}`, width - 200, curY, 16, "blue");
+                }
+                curY += 24;
+            });
         }
     }
 };
 
-// should contain threeapi the gamerenderer & the game scene aswell as the cheat settings
-console.log(window.CheatSDK);
-
 window.addEventListener("keydown", function (event) {
-    for (var table in window.CheatSDK.Settings) {
-        if (window.CheatSDK.Settings.hasOwnProperty(table)) {
-            var current = window.CheatSDK.Settings[table];
+    Game.ForeachSetting(function (current) {
+        if (event.key === current.keybind) {
+            current.enabled = !current.enabled;
 
-            if (event.key === current.keybind) {
-                current.enabled = !current.enabled;
+            if (current.enabled == true && current.onEnabled !== undefined) {
+                current.onEnabled();
+            }
 
-                if (current.enabled == true && current.onEnabled !== undefined) {
-                    current.onEnabled();
-                }
-
-                if (current.enabled == false && current.onDisabled !== undefined) {
-                    current.onDisabled();
-                }
+            if (current.enabled == false && current.onDisabled !== undefined) {
+                current.onDisabled();
             }
         }
-    }
+    })
 });
-
 window.addEventListener("load", function () {
     function onFrame() {
         if (window.CheatSDK.GameRenderer.context === null) {
@@ -59,36 +65,17 @@ window.addEventListener("load", function () {
         }
 
         if (window.CheatSDK.Scene !== undefined) {
-            window.CheatSDK.Scene.traverse(function (object) { // loop over entity(s)
-                if (object.name === "player") {
-                    if (object.renderOrder == 0 && window.CheatSDK.Settings.Chams.enabled) {
-                        object.renderOrder = Infinity;
-                        object.material[0].depthTest = false;
-                        object.material[1].depthTest = false;
-                    }
-                    else if (!window.CheatSDK.Settings.Chams.enabled) {
-                        object.renderOrder = 0;
-                        object.material[0].depthTest = true;
-                        object.material[1].depthTest = true;
-                    }
-                }
-            });
-        }
+            // draw essentials here
+            let width = window.CheatSDK.GameRenderer.canvas.width;
+            let height = window.CheatSDK.GameRenderer.canvas.height;
 
-        // draw essentials here
-        let width = window.CheatSDK.GameRenderer.canvas.width;
-        let height = window.CheatSDK.GameRenderer.canvas.height;
+            window.CheatSDK.GameRenderer.clearCanvas(); // clear last frame drawings
 
-        window.CheatSDK.GameRenderer.clearCanvas(); // clear last frame drawings
-
-        for (var table in window.CheatSDK.Settings) {
-            if (window.CheatSDK.Settings.hasOwnProperty(table)) {
-                var current = window.CheatSDK.Settings[table];
-
+            Game.ForeachSetting(function (current) {
                 if (current.enabled == true && current.onFrame !== undefined) {
                     current.onFrame(width, height); // give canvas dims
                 }
-            }
+            });
         }
 
         requestAnimationFrame(onFrame.bind(this));
@@ -96,3 +83,20 @@ window.addEventListener("load", function () {
 
     onFrame(); // forgot to start the animation loop!!!
 });
+
+let Game = {
+    ForeachSetting: function (callback) {
+        for (var table in window.CheatSDK.Settings) {
+            if (window.CheatSDK.Settings.hasOwnProperty(table)) {
+                callback(window.CheatSDK.Settings[table], table);
+            }
+        }
+    },
+    ForeachPlayer: function (callback) {
+        window.CheatSDK.Scene.traverse(function (object) { // loop over entity(s)
+            if (object.name === "player") {
+                callback(object);
+            }
+        });
+    }
+}
