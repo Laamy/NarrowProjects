51e345f8d33275a480f807b57650fd4bc292d737
yeemi routh <Shade4Keal@gmail.com>
2023-06-01 11:27:38 +1000

    keybind stuff

--- NarrowCheats/src/injected/index.js
+++ NarrowCheats/src/injected/index.js
@@ -1,11 +1,26 @@
 window.CheatSDK.THREE = window.THREE;
 window.CheatSDK.GameRenderer = window.GameRenderer;
 window.CheatSDK.Settings = {
-    Chams: true
+    Chams: {
+        enabled: true,
+        keybind: "z"
+    }
 };
 
 // should contain threeapi the gamerenderer & the game scene aswell as the cheat settings
 console.log(window.CheatSDK);
+
+window.addEventListener("keydown", function (event) {
+    for (var table in window.CheatSDK.Settings) {
+        if (window.CheatSDK.Settings.hasOwnProperty(table)) {
+            var current = window.CheatSDK.Settings[table];
+
+            if (event.key === current.keybind) {
+                current.enabled = !current.enabled;
+            }
+        }
+    }
+});
 
 window.addEventListener("load", function () {
     function onFrame() {
@@ -17,12 +32,12 @@ window.addEventListener("load", function () {
         if (window.CheatSDK.Scene !== undefined) {
             window.CheatSDK.Scene.traverse(function (object) { // loop over entity(s)
                 if (object.name === "player") {
-                    if (object.renderOrder == 0 && window.CheatSDK.Settings.Chams) {
+                    if (object.renderOrder == 0 && window.CheatSDK.Settings.Chams.enabled) {
                         object.renderOrder = Infinity;
                         object.material[0].depthTest = false;
                         object.material[1].depthTest = false;
                     }
-                    else if (!window.CheatSDK.Settings.Chams) {
+                    else if (!window.CheatSDK.Settings.Chams.enabled) {
                         object.renderOrder = 0;
                         object.material[0].depthTest = true;
                         object.material[1].depthTest = true;
